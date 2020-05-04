0fcf9405a949f6a6c68856fc160833c4bcd988da
Ashlen Mencia <ashmencia0103@gmail.com>
2020-05-04 20:10:37 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
 # code here!
+class School
+  attr_accessor:roster
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if @roster[grade]
+      @roster[grade].<<(name)
+    else
+      @roster[grade] = []
+      @roster[grade] << name
+    end
+  end
+end
